8ca75e9468e601f647c7afaf5770f1e9eef0c917
peter samuel <petersamuelyakoub@gmail.com>
2021-12-12 14:26:49 +0200

    first cmmit

---
+++ app/src/androidTest/java/com/church/ministry/MainViewModelTest.kt
@@ -0,0 +1,84 @@
+package com.church.ministry
+
+import androidx.lifecycle.Observer
+import com.church.ministry.data.api.ApiHelper
+import com.church.ministry.data.model.Book
+import com.church.ministry.ui.viewState.MainViewState
+import com.church.ministry.util.NetworkHelper
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+
+class MainViewModelTest {
+
+
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    @Mock
+    private lateinit var apiHelper: ApiHelper
+
+    @Mock
+    private lateinit var databaseHelper: DatabaseHelper
+
+    @Mock
+    private lateinit var apiUsersObserver: Observer<MainViewState.LocalBooksList>
+
+    @Before
+    fun setUp() {
+        // do something if required
+    }
+
+    @Test
+    fun givenServerResponse200_whenFetch_shouldReturnSuccess() {
+        testCoroutineRule.runBlockingTest {
+            doReturn(emptyList<Book>())
+                .`when`(apiHelper)
+                .getBooksList(true, 0)
+            val viewModel = MainViewModel(apiHelper, databaseHelper)
+            viewModel.userIntent.onSend(apiUsersObserver)
+            verify(apiHelper).getBooksList(true, 0)
+            verify(apiUsersObserver).onChanged(MainViewState.LocalBooksList(emptyList()))
+            viewModel.getUsers().removeObserver(apiUsersObserver)
+        }
+    }
+
+    @Test
+    fun givenServerResponseError_whenFetch_shouldReturnError() {
+        testCoroutineRule.runBlockingTest {
+            val errorMessage = "Error Message For You"
+            doThrow(RuntimeException(errorMessage))
+                .`when`(apiHelper)
+                .getUsers()
+            val viewModel = SingleNetworkCallViewModel(apiHelper, databaseHelper)
+            viewModel.getUsers().observeForever(apiUsersObserver)
+            verify(apiHelper).getUsers()
+            verify(apiUsersObserver).onChanged(
+                Resource.error(
+                    RuntimeException(errorMessage).toString(),
+                    null
+                )
+            )
+            viewModel.getUsers().removeObserver(apiUsersObserver)
+        }
+    }
+
+    @After
+    fun tearDown() {
+        // do something if required
+    }
+}
